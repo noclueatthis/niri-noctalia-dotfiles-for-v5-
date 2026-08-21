96efced91b1a93aff51bd65c60509eb77597d36e
no clue <noclueatthis@gmail.com>
2026-08-21 21:50:34 +1000

    Create fish greeting function
    Add custom fish greeting function with fastfetch

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+function fish_greeting
+    set_color white
+    echo '    _   _            _        ___    '
+    echo '   / | / /___  _____/ /_____ _/ (_)___ _'
+    echo '  /  |/ / __ \/ ___/ __/ __ `/ / / __ `/'
+    echo ' / /|  / /_/ / /__/ /_/ /_/ / / / /_/ / '
+    echo '/_/ |_/\____/\___/\__/\__,_/_/_/\__,_/  '
+    set_color normal
+    echo
+
+    fastfetch
+end
